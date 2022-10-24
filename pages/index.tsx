53a5e12aa5caf417c178a9352be7b9e047e859ba
Dominik Borchert <dominik.borchert@lichtblick.de>
2022-10-24 21:14:30 +0200

    React three fiber integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
+import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
   return (
     <Layout pageTitle="Startseite">
-      <div className="container mx-auto">Hello World</div>
+      <Hero />
     </Layout>
   );
 };
