824d6985f4a5c5c923f696d1760d5a3654aaea9e
Dominik Borchert <dominik.borchert@lichtblick.de>
2022-10-24 23:03:53 +0200

    Physics

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,36 +1,104 @@
+import { useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { EffectComposer, SSAO } from "@react-three/postprocessing";
+import type { PlaneProps, Triplet } from "@react-three/cannon";
 
-function Box(props: ThreeElements["mesh"]) {
-  const ref = useRef<THREE.Mesh>(null!);
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+import { Physics, useBox, usePlane, useSphere } from "@react-three/cannon";
+import niceColors from "nice-color-palettes";
+
+import { InstancedMesh, Mesh, Vector3 } from "three";
+import { Color } from "three";
+
+function Rig({ v = new THREE.Vector3() }) {
+  return useFrame((state) => {
+    state.camera.position.lerp(
+      v.set(state.mouse.x / 2, state.mouse.y / 2 + 1, 2),
+      0.05
+    );
+  });
+}
+
+function Plane(props: PlaneProps) {
+  const [ref] = usePlane(() => ({ ...props }), useRef<Mesh>(null));
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    <mesh ref={ref} receiveShadow>
+      <planeBufferGeometry args={[5, 5]} />
+
+      <shadowMaterial
+        color={new Color("#B1BD00").convertSRGBToLinear()}
+        opacity={0.125}
+      />
     </mesh>
   );
 }
 
+type InstancedGeometryProps = {
+  number: number;
+  size: number;
+};
+
+const Boxes = ({ number, size }: InstancedGeometryProps) => {
+  const args: Triplet = [size, size, size];
+  const [ref, { at }] = useBox(
+    () => ({
+      args,
+      mass: 1,
+      position: [Math.random() - 0.5, Math.random() * 2, Math.random() - 0.5],
+    }),
+    useRef<InstancedMesh>(null)
+  );
+  useFrame(() =>
+    at(Math.floor(Math.random() * number)).position.set(0, Math.random() * 2, 0)
+  );
+  return (
+    <instancedMesh
+      receiveShadow
+      castShadow
+      ref={ref}
+      args={[undefined, undefined, number]}
+    >
+      <boxBufferGeometry args={args}></boxBufferGeometry>
+      <meshLambertMaterial color={new Color("#F6FD8C").convertSRGBToLinear()} />
+    </instancedMesh>
+  );
+};
+
 const Hero = () => {
+  const [number] = useState(200);
+  const [size] = useState(0.1);
+
   return (
     <div className="h-screen w-full">
       <aside className="absolute w-full h-full">
-        <Canvas>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
+        <Canvas
+          shadows
+          onCreated={({ scene }) => (scene.background = new Color("#FDFFF5"))}
+        >
+          <hemisphereLight intensity={0.65} />
+          <spotLight
+            position={[5, 5, 5]}
+            angle={0.3}
+            penumbra={1}
+            intensity={1}
+            castShadow
+            shadow-mapSize-width={256}
+            shadow-mapSize-height={256}
+          />
+          <Rig />
+          <EffectComposer multisampling={0}>
+            <SSAO
+              samples={11}
+              radius={0.1}
+              intensity={50}
+              luminanceInfluence={0.6}
+              color="#B1BD00"
+            />
+          </EffectComposer>
+          <Physics broadphase="SAP">
+            <Plane rotation={[-Math.PI / 2, 0, 0]} />
+            <Boxes {...{ number, size }} />
+          </Physics>
         </Canvas>
       </aside>
     </div>
